f34b83ee9e7f3b2c83401c245814063bd2c9f8d0
yangcmpk <MLS@2014-467deMacBook-Pro.local>
2015-02-02 23:47:57 +0800

    update
    数据库里存的时间是日志时间。更改代码逻辑，写入两天前数据，没有的数据用0补充，更新四天前为0的数据。

--- MLS/statistics_demand_2015-01-27/put_resutl_2_mysql.py
+++ MLS/statistics_demand_2015-01-27/put_resutl_2_mysql.py
@@ -3,6 +3,9 @@
 
 import sys
 import os
+import time
+import datetime
+import string
 from common.statutil import get_stat_db, get_config_db
 
 '''
@@ -16,26 +19,35 @@ all_data_list_comment = ['goods_initiative_comment','goods_all_comment','order_a
 #     avg(fast),avg(accord),avg(quality),avg(attitude),avg((fast+accord+quality+attitude)/4)
 # 退货 的列表
 all_data_list_refund = ['mount_youliyou','mount_wuliyou','mount_youliyou_success','mount_wuliyou_success','money_youliyou','money_wuliyou','money_youliyou_success','money_wuliyou_success','mount_youliyou_success_90day','mount_wuliyou_success_90day','mount_all_order_90day','money_youliyou_success_90day','money_wuliyou_success_90day','money_all_order_90day']
-# 发货 的列表
-all_data_list_send = ['send_before_refund','all_order_fahuo_xiayu_xiadan_72hour','wuliu_dayu_chengjiao_xiaoyu_72','avg_sendtime_paytime','avg_expresstime_paytime','wu_wuliu','wuliu_zaoyu_chengjiao']
+# 发货 的列表 全表的表头，还有需要写入四天前和两天前的列表
+# all_data_list_send = ['send_before_refund','all_order_fahuo_xiayu_xiadan_72hour','wuliu_dayu_chengjiao_xiaoyu_72','avg_sendtime_paytime','avg_expresstime_paytime','wu_wuliu','wuliu_zaoyu_chengjiao']
+all_data_list_send_four_day_ago = ['send_before_refund','all_order_fahuo_xiayu_xiadan_72hour','wuliu_dayu_chengjiao_xiaoyu_72','avg_sendtime_paytime','avg_expresstime_paytime']
+# mysql的列名
+all_data_list_send_four_day_ago_mysql = ['no_refund_before_delivery_in72hours_after_paid_4day_ago','goods_quantity_send_in_72hour_after_pay_4day_ago','goods_quantity_logistics_in_72hour_after_pay_4day_ago','avg_sendtime_4day_ago','avg_logisticstime_4day_ago']
+all_data_list_send_two_day_ago = ['wu_wuliu','wuliu_zaoyu_chengjiao']
 
 # 存全部数据都的字典
 all_data_dict = {}
 # 日期经过转化成UNIXtime，作为主键
 id = 0
-# 时间，MySQL中查找使用
+# 时间，MySQL中查找使用 传的不是日志时间，而是真实时间，也就是日志时间的后一天。在脚本里计算2天前4天前这样的记录时间
 insert_date = ''
+one_day_ago_datetime = ''
+two_day_ago_datetime = ''
+four_day_ago_datetime = ''
 # 结果文件名
 file_name = ''
 
 def init_data_dict ():
     # 用列表初始化字典，全部数据设置为 -1 ，这样结果里如果有 -1 ，证明之前的计算逻辑出了问题了
     for i in all_data_list_comment:
-        all_data_dict[i]=-1
+        all_data_dict[i] = "0"
     for i in all_data_list_refund:
-        all_data_dict[i]=-1
-    for i in all_data_list_send:
-        all_data_dict[i]=-1
+        all_data_dict[i] = "0"
+    for i in all_data_list_send_four_day_ago:
+        all_data_dict[i] = "0"
+    for i in all_data_list_send_two_day_ago:
+        all_data_dict[i] = "0"
 
 def get_data_from_file():
     # 从文件中获取出全部数据，存入字典中
@@ -51,11 +63,13 @@ def get_data_from_file():
             print "key : " + k + " not in list"
     pass
 
-def exec_stat_sql(table_name,all_values):
+# 插入的表名字、值、作为主键的时间戳、时间字段
+def exec_stat_sql(table_name,all_values,true_insert_date):
     try:
         cur = get_config_db('dolphin_stat',1)
-        delete_sql = "delete from %s where id = %s" % (table_name,id)
-        insert_sql = "insert into %s () values(%s,'%s',%s)" % (table_name,id,insert_date,all_values)
+        true_id = int( time.mktime(time.strptime(true_insert_date,'%Y-%m-%d')) )
+        delete_sql = "delete from %s where id = %s" % (table_name,true_id)
+        insert_sql = "insert into %s () values(%s,'%s',%s)" % (table_name,true_id,true_insert_date,all_values)
         print delete_sql
         cur.execute(delete_sql)
         print insert_sql
@@ -63,6 +77,19 @@ def exec_stat_sql(table_name,all_values):
     except Exception, e:
         print "execute ERROR, Exception ", e
         # time.sleep(10)
+
+# 更新表的SQL。all_values 必须是key = value 这样的形式
+def exec_stat_update_sql(table_name,all_values,true_insert_date):
+    try:
+        cur = get_config_db('dolphin_stat',1)
+        true_id = int( time.mktime(time.strptime(true_insert_date,'%Y-%m-%d')) )
+        update_sql = "update %s set %s where id = %s" % (table_name,all_values,true_id)
+        print update_sql
+        cur.execute(update_sql)
+    except Exception, e:
+        print "execute ERROR, Exception ", e
+        # time.sleep(10)
+
 
 def show_all_data():
     # 按三个表分别拼数据然后写入MySQL
@@ -78,7 +105,7 @@ def show_all_data():
         #  print i,all_data_dict[i]
     # print all_keys
     # print all_values
-    exec_stat_sql('t_dolphin_stat_comment',all_values)
+    exec_stat_sql('t_dolphin_stat_comment',all_values,one_day_ago_datetime)
     # print sql
     print "==============退款 table t_dolphin_stat_refund"
     all_keys=""
@@ -92,20 +119,34 @@ def show_all_data():
         # print i,all_data_dict[i]
     # print all_keys
     # print all_values
-    exec_stat_sql('t_dolphin_stat_refund',all_values)
+    exec_stat_sql('t_dolphin_stat_refund',all_values,one_day_ago_datetime)
     print "==============发货 table t_dolphin_stat_goods_delivery"
     all_keys=""
     all_values=""
-    for i in all_data_list_send:
+    for i in all_data_list_send_four_day_ago + all_data_list_send_two_day_ago:
         if (all_keys != ""):
             all_keys += ","
             all_values += ","
         all_keys += i
-        all_values += all_data_dict[i]
+        if i in all_data_list_send_four_day_ago:
+            all_values += "0"
+        else:
+            all_values += all_data_dict[i]
         # print i,all_data_dict[i]
     # print all_keys
     # print all_values
-    exec_stat_sql('t_dolphin_stat_goods_delivery',all_values)
+    exec_stat_sql('t_dolphin_stat_goods_delivery',all_values,two_day_ago_datetime)
+    # 更新4天前的数据
+    all_keys=""
+    all_values=""
+    for i in [0,1,2,3,4]:
+        if (all_keys != ""):
+            all_keys += ","
+            all_values += ","
+        all_keys += all_data_list_send_four_day_ago_mysql[i]
+        all_values += all_data_list_send_four_day_ago_mysql[i] + "=" +all_data_dict[all_data_list_send_four_day_ago[i]]
+        # print i,all_data_dict[i]
+    exec_stat_update_sql('t_dolphin_stat_goods_delivery',all_values,four_day_ago_datetime)
 
 
 if __name__ == "__main__":
@@ -117,6 +158,14 @@ if __name__ == "__main__":
     # print file_name
     # print insert_date
     # print id
+    # print time.strptime(insert_date,'%Y-%m-%d').tm_mday + time.strptime(insert_date,'%Y-%m-%d').tm_mday 
+    insert_datetime = datetime.datetime(string.atoi(insert_date.split("-")[0]),string.atoi(insert_date.split("-")[1]),string.atoi(insert_date.split("-")[2]))
+    date_tmp = -1
+    one_day_ago_datetime = ( insert_datetime + datetime.timedelta(days = date_tmp) ).strftime("%Y-%m-%d") 
+    date_tmp = -2
+    two_day_ago_datetime = ( insert_datetime + datetime.timedelta(days = date_tmp) ).strftime("%Y-%m-%d") 
+    date_tmp = -4
+    four_day_ago_datetime = ( insert_datetime + datetime.timedelta(days = date_tmp) ).strftime("%Y-%m-%d") 
 
     init_data_dict()
     get_data_from_file()
